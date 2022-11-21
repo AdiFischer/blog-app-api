f737d805c13a138e2226dab534ab343ffb50f0e8
AdiFischer <adifischer1@gmail.com>
2022-11-21 11:14:15 -0500

    insertOne fixed

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 import express from "express";
 import cors from "cors"
+import { MongoClient } from 'mongodb'
+import 'dotenv/config'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
+const client = new MongoClient(process.env.MONGO_URI)//MC-class, imported and make a new instance of it
+const database = client.db('blog')
+const posts = database.collection('posts')
+
+client.connect()
+console.log('connected to mongo')
 
 
-app.get('/', (req, res) => {
-    res.send('hello')
+// app.get('/', (req, res) => {
+//     res.send('hello')
+// })
+
+
+app.listen(process.env.PORT, () => console.log(`listening to http://localhost:${process.env.PORT}....`))
+
+//post - add 1 post item
+//----- author ,date, text
+app.post('/', async(req, res) => {
+    // const newPost = {author: 'Jon', date: '2022-21-22', text: 'blah blah blah'}
+    // console.log('req.body ->', req.body)
+    
+    await posts.insertOne(req.body)
+    res.send('item added')
 })
-const PORT = 3030
 
-app.listen(PORT, () => console.log(`listening to http://localhost:${PORT}....`))
+
+//get all posts
